47e02101d9ffda7c6cf3ab86f7c7049a2b6aa5d1
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-02-26 18:27:24 -0600

    begin implementing options whitelist

--- src/options/twitter-dashboard-options.js
+++ src/options/twitter-dashboard-options.js
@@ -3,23 +3,47 @@
     const config = {
         debug: true,
         selectors: {
-            default: '#options-content'
+            default: '#options-header',
+            whitelist: '#whitelist',
+            save: '#save'
         }
     }
 
-    function options() {
+    function formatWhitelist(text) {
+        let ret = text.trim()
+                      .replace(/\r?\n|\r/g, '')
+                      .replace(/[\n\r\t]/gm, '')
+                      .replace(/['"]+/g, '')
+                      .replace(/[ ]/g, '')
+        return ret
+    }
+
+    function saveOptions() {
+        const whitelistText = formatWhitelist(document.querySelector(config.selectors.whitelist).value)
+        const whitelistArray = whitelistText.split(',')
+        const options = {
+            whitelist: whitelist
+        }
+        browser.storage.local.set(options)
+    }
+
+    function egg() {
         const menuItem = document.querySelector(config.selectors.default)
         menuItem.innerHTML =
             `<img src="../icons/twitter-dashboard-96.png"></img>`
     }
 
-    function addClickListener() {
-        const menuItem = document.querySelector(config.selectors.default)
-        menuItem.addEventListener('click', (e) => {
-            options()
+    function addClickListeners() {
+        const save = document.querySelector(config.selectors.save)
+        save.addEventListener('click', (e) => {
+            saveOptions();
+        })
+        const title = document.querySelector(config.selectors.default)
+        title.addEventListener('click', (e) => {
+            egg()
         })
     }
 
-    addClickListener()
+    addClickListeners()
 
 })();
