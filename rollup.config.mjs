f0efaaa2c7e837bb3b005296da5fb447dbdede4b
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-03-03 01:19:58 -0600

    update rollup config to bundle other interface components

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,8 +5,9 @@
  */
 import resolve from '@rollup/plugin-node-resolve'
 import cjs from '@rollup/plugin-commonjs'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
-export default {
+export default [{
     input: 'src/twitter-dashboard.js',
     output: {
         file: 'dist/rolledup/twitter-dashboard.js',
@@ -17,6 +18,24 @@ export default {
         // resolve node imports
         resolve(),
         // convert commonjs to es6
-        cjs()
+        cjs(),
+        // fill import dependencies
+        nodePolyfills()
     ]
-}
+},{
+    input: 'src/options/twitter-dashboard-options.js',
+    output: {
+        file: 'dist/rolledup/options/twitter-dashboard-options.js',
+        format: 'iife',
+        name: 'twitterDashboardOptions'
+    },
+    plugins: [resolve(), cjs(), nodePolyfills()]
+},{
+    input: 'src/popup/twitter-dashboard-popup.js',
+    output: {
+        file: 'dist/rolledup/popup/twitter-dashboard-popup.js',
+        format: 'iife',
+        name: 'twitterDashboardPopup'
+    },
+    plugins: [resolve(), cjs(), nodePolyfills()]
+}]
